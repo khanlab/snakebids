bdf2eb985227aa6e240cf386cd49a8cebc68715b
Ali Khan <alik@robarts.ca>
2020-11-16 09:26:16 -0500

    increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="snakebids", 
-    version="0.1.3",
+    version="0.1.4",
     author="Ali Khan",
     author_email="alik@robarts.ca",
     description="BIDS integration into snakemake workflows",
