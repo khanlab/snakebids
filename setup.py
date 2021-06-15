2cd43af926cbed02cef5b40e30fd51ebc006e0a7
BumpVersion Action <bumpversion@github-actions>
2021-06-15 18:05:10 +0000

    Bump version: 0.3.10 → 0.3.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="snakebids",
-    version="0.3.10",
+    version="0.3.11",
     author="Ali Khan",
     author_email="alik@robarts.ca",
     description="BIDS integration into snakemake workflows",
