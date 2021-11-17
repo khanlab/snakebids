c42567543100a3c4ee4871968fc3f4c9ada47ba6
Ali Khan <alik@robarts.ca>
2021-11-17 11:53:35 -0500

    fix for black formatting

--- snakebids/fix_dd.py
+++ snakebids/fix_dd.py
@@ -7,9 +7,13 @@ from pathlib import Path
 def get_parser():
     parser = argparse.ArgumentParser(
         description="Update BIDS Derivatives Dataset  to be backwards compatible with pybids",
-        epilog=("Adds PipelineDescription.Name to the dataset_description.json file, from earlier BIDS spec) that pybids requires, using the first entry of the GeneratedBy list.  This is needed if parsing a modern version of fmriprep derivatives with snakebids/pybids."),
+        epilog=(
+            "Adds PipelineDescription.Name to the dataset_description.json file, from earlier BIDS spec) that pybids requires, using the first entry of the GeneratedBy list.  This is needed if parsing a modern version of fmriprep derivatives with snakebids/pybids."
+        ),
     )
-    parser.add_argument("dataset_root","dataset-root", help="BIDS derivative dataset to fix")
+    parser.add_argument(
+        "dataset_root", "dataset-root", help="BIDS derivative dataset to fix"
+    )
     return parser
 
 
