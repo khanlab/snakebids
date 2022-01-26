89ed53b371e49b3b34a976af2b71e9ea18159116
Ali Khan <alik@robarts.ca>
2022-01-25 21:11:57 -0500

    update paths, and quality checks
    skipping docs inside cookiecutter for flake8

--- snakebids/project_template/{{cookiecutter.app_name}}/setup.py
+++ snakebids/project_template/{{cookiecutter.app_name}}/setup.py
@@ -1,18 +1,19 @@
-import setuptools
 import json
+
+import setuptools
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
-with open('pipeline_description.json', 'r') as fh:
+with open("pipeline_description.json", "r") as fh:
     pipeline = json.load(fh)
-    name = pipeline['GeneratedBy'][0]['Name']
-    description = pipeline['Name']
-    version = pipeline['GeneratedBy'][0]['Version']
-    url = pipeline['GeneratedBy'][0]['CodeURL']
-    author = pipeline['GeneratedBy'][0]['Author']
-    author_email = pipeline['GeneratedBy'][0]['AuthorEmail']
- 
+    name = pipeline["GeneratedBy"][0]["Name"]
+    description = pipeline["Name"]
+    version = pipeline["GeneratedBy"][0]["Version"]
+    url = pipeline["GeneratedBy"][0]["CodeURL"]
+    author = pipeline["GeneratedBy"][0]["Author"]
+    author_email = pipeline["GeneratedBy"][0]["AuthorEmail"]
+
 setuptools.setup(
     name=name,
     version=version,
@@ -29,12 +30,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points={'console_scripts': [
-        '{{cookiecutter.app_name}}={{cookiecutter.app_name}}.run:main'
-    ]},
-    install_requires=[
-        "snakebids>={{cookiecutter.snakebids_version}}",
-        "snakemake"
-    ],
-    python_requires='>=3.7'
+    entry_points={
+        "console_scripts": [
+            "{{cookiecutter.app_name}}={{cookiecutter.app_name}}.run:main"
+        ]
+    },
+    install_requires=["snakebids>={{cookiecutter.snakebids_version}}", "snakemake"],
+    python_requires=">=3.7",
 )
--- snakebids/project_template/{{cookiecutter.app_name}}/{{cookiecutter.app_name}}/run.py
+++ snakebids/project_template/{{cookiecutter.app_name}}/{{cookiecutter.app_name}}/run.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 from pathlib import Path
+
 from snakebids.app import SnakeBidsApp
 
 
 def get_parser():
     """Exposes parser for sphinx doc generation, cwd is the docs dir"""
-    app = SnakeBidsApp('../',skip_parse_args=True)
+    app = SnakeBidsApp("../", skip_parse_args=True)
     return app.parser
 
 
 def main():
-    app = SnakeBidsApp(Path(__file__).resolve().parents[1]) # to get repository root
+    app = SnakeBidsApp(Path(__file__).resolve().parents[1])  # to get repository root
     app.run_snakemake()
 
 
