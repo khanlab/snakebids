2254d2204123f63110dc891c40055ca16c7b819c
Peter Van Dyken <pvandyk2@uwo.ca>
2022-06-17 11:27:12 -0400

    Refactor bids strategy code
    - Move all bids-related derived strategies to new strategies module
    - Collect all references to st.text(BIDS_ALPHABET) into a single
      bids_value() strategy
    - Move helper functions into helper module
    - Maximum sizes have been added to most lists to limit combinatorial
      explosion

--- snakebids/tests/helpers.py
+++ snakebids/tests/helpers.py
@@ -2,7 +2,42 @@
 """
 
 from collections import UserDict
-from typing import Dict
+from typing import Any, Dict, Iterable, List, Tuple
+
+
+def get_zip_list(entities: Iterable[str], combinations: Iterable[Tuple[str, ...]]):
+    """Return a zip list from iterables of entities and value combinations
+
+    Parameters
+    ----------
+    entities : Iterable[str]
+        Iterable of entities
+    combinations : Iterable[Tuple[str, ...]]
+        Iterable of combinations (e.g. produced using itertools.product). Each tuple
+        must be the same length as entities
+
+    Returns
+    -------
+    Dict[str, List[str]]
+        zip_list representation of entity-value combinations
+    """
+    return {entity: list(combs) for entity, combs in zip(entities, zip(*combinations))}
+
+
+def setify(dic: Dict[Any, List[Any]]):
+    """Convert a dict of *->lists into a dict of *->sets
+
+    Parameters
+    ----------
+    dic : Dict[Any, List[Any]]
+        Dict of lists to convert
+
+    Returns
+    -------
+    Dict[Any, Set[Any]]
+        Dict of sets
+    """
+    return {key: set(val) for key, val in dic.items()}
 
 
 class BidsListCompare(UserDict):
