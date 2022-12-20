c2c8edcf0a973b313403521003bb3b70ed076bf8
Peter Van Dyken <pvandyk2@uwo.ca>
2022-12-20 11:56:00 -0500

    Refactor testing libraries
    Move re-usable components to the test helpers and conftest modules

--- snakebids/tests/strategies.py
+++ snakebids/tests/strategies.py
@@ -1,10 +1,14 @@
+import copy
 import itertools as it
+from pathlib import Path
 from typing import Any, List, Optional, Type
 
 import hypothesis.strategies as st
 from bids.layout import Config as BidsConfig
+from hypothesis import assume
 
 from snakebids.core.datasets import BidsComponent
+from snakebids.core.input_generation import BidsDataset
 from snakebids.tests import helpers
 from snakebids.utils.utils import BidsEntity
 
@@ -80,6 +84,7 @@ def bids_components(
     min_size: int = 1,
     max_size: int = 5,
     entities: Optional[List[BidsEntity]] = None,
+    root: Optional[Path] = None,
     restricted_chars: bool = False,
 ):
     zip_lists = draw(
@@ -91,11 +96,11 @@ def bids_components(
         )
     )
 
-    path = helpers.get_bids_path(zip_lists)
+    path = (root or Path()) / helpers.get_bids_path(zip_lists)
 
     return BidsComponent(
         name=draw(bids_value()),
-        path=path,
+        path=str(path),
         zip_lists=zip_lists,
     )
 
@@ -125,3 +130,31 @@ def everything_except(*excluded_types: Type[Any]):
         .flatmap(st.from_type)
         .filter(lambda s: not isinstance(s, excluded_types))
     )
+
+
+@st.composite
+def datasets(draw: st.DrawFn, root: Optional[Path] = None):
+    ent1 = draw(bids_entity_lists(min_size=2, max_size=3))
+    assume("datatype" not in ent1)
+    # Currently, space and ce cannot coexist because ce is a substr of space (see
+    # snakebids.core.input_generation:_parse_bids_path)
+    assume(not ("space" in ent1 and "ceagent" in ent1))
+    ent2 = copy.copy(ent1)
+    ent2.pop()
+    # BUG: snakebids currently doesn't properly parse paths with just suffix
+    assume(ent2 != ["suffix"])
+    comp1 = draw(bids_components(entities=ent1, restricted_chars=True, root=root))
+    comp2 = draw(bids_components(entities=ent2, restricted_chars=True, root=root))
+    assume(comp1.input_name != comp2.input_name)
+    return BidsDataset.from_iterable([comp1, comp2])
+
+
+@st.composite
+def datasets_one_comp(draw: st.DrawFn, root: Optional[Path] = None):
+    ent1 = draw(bids_entity_lists(min_size=2, max_size=3))
+    assume("datatype" not in ent1)
+    # Currently, space and ce cannot coexist because ce is a substr of space (see
+    # snakebids.core.input_generation:_parse_bids_path)
+    assume(not ("space" in ent1 and "ceagent" in ent1))
+    comp1 = draw(bids_components(entities=ent1, restricted_chars=True, root=root))
+    return BidsDataset.from_iterable([comp1])
