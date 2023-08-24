3a25e9974d5ef13d63b9b84c84155d3023b40ce4
Peter Van Dyken <pvandyk2@uwo.ca>
2023-08-24 16:53:27 -0400

    Add deprecation mechanism to old pybidsdb vals
    Pybidsdb values encoded in the config file had their key names changed
    without warning in snakebids==0.9.0. This caused apps relying on those
    configuration names to silently fail to read the database
    
    With this fix, once again set the keys with parsable sentinal values so
    that reliant workflows will still work but maintainers will be alerted
    to the deprecation
    
    Resolves #333

--- snakebids/utils/utils.py
+++ snakebids/utils/utils.py
@@ -50,6 +50,10 @@ class BidsTag(TypedDict):
 BidsTags: TypeAlias = "dict[str, BidsTag]"
 
 
+DEPRECATION_FLAG = "<!DEPRECATED!>"
+"""Sentinel string to mark deprecated config features"""
+
+
 @ft.lru_cache(None)
 def read_bids_tags(bids_json: Path | None = None) -> BidsTags:
     """Read the bids tags we are aware of from a JSON file.
