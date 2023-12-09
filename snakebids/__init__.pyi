49077a691d3c919d0d6c75328e8a2c87c426483c
Peter Van Dyken <pvandyk2@uwo.ca>
2023-12-08 22:58:27 -0500

    Add v0.10.1 spec
    Incorporates all of the bids 1.9.0 entities, as well as from, to, and
    any other entities currently configured in pybids. Long and short names
    are used for all relevant entities. Unknown entities are placed just
    before description

--- snakebids/__init__.pyi
+++ snakebids/__init__.pyi
@@ -11,8 +11,11 @@ from .core import (
     write_derivative_json,
 )
 from .paths import (
+    BidsFunction,
     bids,
+    bids_factory,
     bids_v0_0_0,
+    bids_v0_10_1,
 )
 
 __all__ = [
@@ -20,9 +23,12 @@ __all__ = [
     "BidsComponentRow",
     "BidsDataset",
     "BidsDatasetDict",
+    "BidsFunction",
     "BidsPartialComponent",
     "bids",
+    "bids_factory",
     "bids_v0_0_0",
+    "bids_v0_10_1",
     "filter_list",
     "generate_inputs",
     "get_filtered_ziplist_index",
--- snakebids/paths/__init__.py
+++ snakebids/paths/__init__.py
@@ -1,10 +1,10 @@
 # type: ignore
-__submodules__ = ["presets"]
+__submodules__ = ["factory", "presets"]
 
 # <AUTOGEN_INIT>
 import lazy_loader
 
 __getattr__, __dir__, __all__ = lazy_loader.attach_stub(__name__, __file__)
 
-__all__ = ["bids", "bids_v0_0_0"]
+__all__ = ["BidsFunction", "bids", "bids_factory", "bids_v0_0_0", "bids_v0_10_1"]
 # </AUTOGEN_INIT>
