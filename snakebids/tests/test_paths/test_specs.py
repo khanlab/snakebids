14ecc4b8d9b96a1cec258743335b9ed51874fc97
Peter Van Dyken <pvandyk2@uwo.ca>
2023-12-11 14:50:14 -0500

    Add deprecation and dangerous bids usage warnings
    Warn if `use_subject_dir` or `use_session_dir` is specified, these will
    be eliminated in 1.0
    
    Warn if custom entitites are used in a spec when no spec is explicitly
    identified

--- snakebids/tests/test_paths/test_specs.py
+++ snakebids/tests/test_paths/test_specs.py
@@ -1,5 +1,10 @@
-from snakebids.paths import specs
-from snakebids.paths._config import set_bids_spec
+import warnings
+
+import pytest
+from pytest_mock import MockerFixture
+
+from snakebids.paths import bids_factory, specs
+from snakebids.paths._config import reset_bids_spec, set_bids_spec
 from snakebids.paths._presets import bids
 from snakebids.paths._utils import find_entity
 from snakebids.paths.specs import v0_0_0
@@ -35,3 +40,39 @@ def test_spec_can_be_set_with_obj():
     assert bids(acquisition="foo") == "acquisition-foo"
     set_bids_spec(specs.v0_10_1())
     assert bids(acquisition="foo") == "acq-foo"
+
+
+def test_using_include_subject_dir_raises_warning():
+    with pytest.warns(UserWarning, match="include_session_dir and include_subject_dir"):
+        bids(subject="001", include_subject_dir=False)
+    with pytest.warns(UserWarning, match="include_session_dir and include_subject_dir"):
+        bids(session="001", include_session_dir=False)
+
+
+class TestCustomEntityWarnings:
+    def test_using_custom_entities_with_default_bids_raises_warning(self):
+        reset_bids_spec()
+        with pytest.warns(UserWarning, match="spec has not been explicitly declared"):
+            bids(foo="bar")
+
+    def test_no_warning_when_spec_declared(self):
+        reset_bids_spec()
+        set_bids_spec("v0_0_0")
+        with warnings.catch_warnings():
+            warnings.simplefilter("error")
+            bids(foo="bar")
+
+    def test_no_warning_when_bids_explicitly_generated(self):
+        reset_bids_spec()
+        with warnings.catch_warnings():
+            warnings.simplefilter("error")
+            bids_factory(specs.v0_0_0())(foo="bar")
+
+    def test_no_warning_in_interactive_mode(self, mocker: MockerFixture):
+        reset_bids_spec()
+        mocker.patch(
+            "snakebids.paths._factory.in_interactive_session", return_value=True
+        )
+        with warnings.catch_warnings():
+            warnings.simplefilter("error")
+            bids(foo="bar")
