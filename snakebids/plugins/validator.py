9fe8023a03d4f68c0d922c04763d8d50e494f52a
Peter Van Dyken <pvandyk2@uwo.ca>
2024-07-13 14:25:28 -0400

    Add CLI argument in BidsValidator plugin

--- snakebids/plugins/validator.py
+++ snakebids/plugins/validator.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import argparse
 import json
 import logging
 import subprocess as sp
@@ -10,6 +11,8 @@ import attr
 
 from snakebids import bidsapp
 from snakebids.exceptions import SnakebidsPluginError
+from snakebids.plugins.base import PluginBase
+from snakebids.utils.utils import text_fold
 
 _logger = logging.getLogger(__name__)
 
@@ -19,11 +22,19 @@ class InvalidBidsError(SnakebidsPluginError):
 
 
 @attr.define
-class BidsValidator:
+class BidsValidator(PluginBase):
     """Perform validation of a BIDS dataset using the bids-validator.
 
     If the dataset is not valid according to the BIDS specifications, an
     InvalidBidsError is raised.
+
+    An argument --skip-bids-validation is added to the CLI. This argument can be
+    overriden by adding an argument with dest ``plugins.validator.skip`` before this
+    plugin runs.
+
+    Two entries are added to config:
+    - ``"plugins.validator.skip"``: Indicates if validation was skipped
+    - ``"plugins.validator.success"``: Indicates if validation succeeded
 
     Parameters
     ----------
@@ -35,8 +46,29 @@ class BidsValidator:
 
     raise_invalid_bids: bool = attr.field(default=True)
 
+    PREFIX = "plugins.validator"
+
     def __eq__(self, other: Any):
         return isinstance(other, self.__class__)
+
+    @bidsapp.hookimpl
+    def add_cli_arguments(self, parser: argparse.ArgumentParser):
+        """Add option to skip validation."""
+        self.try_add_argument(
+            parser,
+            "--skip-bids-validation",
+            dest="skip",
+            action="store_true",
+            default=False,
+            help=text_fold(
+                """
+                Skip validation of BIDS dataset. BIDS validation is performed by default
+                using the bids-validator plugin (if installed/enabled) or with the
+                pybids validator implementation (if bids-validator is not
+                installed/enabled).
+                """
+            ),
+        )
 
     @bidsapp.hookimpl
     def finalize_config(self, config: dict[str, Any]) -> None:
