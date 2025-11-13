60e9e8209d8488e6fd6ea19cbb058b714cbe5744
Peter Van Dyken <pvandyk2@uwo.ca>
2025-11-13 18:07:56 -0500

    Refactor querying tests
    Previous tests of filtering relied on costly generation of components
    with hypothesis combined with fs read/writes from pybids. This took an
    excessively long time.
    
    Fix by adding a new suite of focused unit tests of the query module. The
    old integration tests are simplified using parametrize rather than
    hypothesis.

--- src/snakebids/core/_querying.py
+++ src/snakebids/core/_querying.py
@@ -17,6 +17,8 @@ from snakebids.types import FilterMap, FilterValue, InputConfig
 
 CompiledFilter: TypeAlias = "Mapping[str, Sequence[str | Query]]"
 
+_VALID_FILTER_METHODS = frozenset(("get", "match", "search"))
+
 
 class PostFilter:
     """Filters to apply after indexing, typically derived from the CLI.
@@ -27,6 +29,13 @@ class PostFilter:
     def __init__(self):
         self.inclusions: dict[str, Sequence[str] | str] = {}
         self.exclusions: dict[str, Sequence[str] | str] = {}
+
+    def __eq__(self, other: object):
+        if not isinstance(other, self.__class__):
+            return False
+        return (
+            self.inclusions == other.inclusions and self.exclusions == other.exclusions
+        )
 
     def add_filter(
         self,
@@ -60,19 +69,25 @@ class PostFilter:
         """
         if inclusions is not None:
             self.inclusions[key] = list(itx.always_iterable(inclusions))
-        if exclusions is not None:
-            self.exclusions[key] = self._format_exclusions(exclusions)
+        if exclusions is not None and (
+            (formatted := self._format_exclusions(exclusions)) is not None
+        ):
+            self.exclusions[key] = formatted
 
     def _format_exclusions(self, exclusions: Iterable[str] | str):
+        hit = None
         # if multiple items to exclude, combine with with item1|item2|...
         exclude_string = "|".join(
-            re.escape(label) for label in itx.always_iterable(exclusions)
+            hit := re.escape(label) for label in itx.always_iterable(exclusions)
         )
+        if hit is None:
+            return None
         # regex to exclude subjects
-        return [f"^((?!({exclude_string})$).*)$"]
+        # ^ (NOT: (item1|item2|...)$) ANYTHING...
+        return [f"^(?!(?:{exclude_string})$).*"]
 
 
-@attrs.define(slots=False)
+@attrs.define
 class UnifiedFilter:
     """Manages component level and post filters."""
 
@@ -94,7 +109,7 @@ class UnifiedFilter:
     @classmethod
     def from_filter_dict(
         cls,
-        filters: Mapping[str, str | bool | Sequence[str | bool]],
+        filters: FilterMap,
         postfilter: PostFilter | None = None,
     ) -> Self:
         """Patch together a UnifiedFilter based on a basic filter dict.
@@ -246,6 +261,7 @@ def get_matching_files(
         raise PybidsError(msg) from err
 
     if search is not None:
+        # intersection preserving order
         return [p for p in get if p in search]
     return get
 
@@ -338,11 +354,11 @@ def _compile_filters(filters: FilterMap, *, with_regex: bool) -> CompiledFilter:
             filt = raw_filter
             filt_type = "get"
         else:
-            if filt_type not in {"match", "search", "get"}:
+            if filt_type not in _VALID_FILTER_METHODS:
                 raise _InvalidKeyError(key, raw_filter)
             if TYPE_CHECKING:
                 assert isinstance(raw_filter, dict)
-            filt = cast("str | bool | Sequence[str | bool]", raw_filter[filt_type])
+            filt = raw_filter[filt_type]  # pyright: ignore[reportTypedDictNotRequiredAccess]
 
         # these two must not be simultaneously true or false
         if with_regex == (filt_type == "get"):
