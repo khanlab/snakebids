7c7ff571ffd02874fea444baac00da6c8c0aa83f
Peter Van Dyken <pvandyk2@uwo.ca>
2025-11-13 18:07:56 -0500

    Return empty list when PostFilter exclusions empty
    Previously returned a regex with a negative look-ahead on an empty
    string. This would match and exclude everything.

--- src/snakebids/core/_querying.py
+++ src/snakebids/core/_querying.py
@@ -63,13 +63,16 @@ class PostFilter:
         if exclusions is not None:
             self.exclusions[key] = self._format_exclusions(exclusions)
 
-    def _format_exclusions(self, exclusions: Iterable[str] | str):
+    def _format_exclusions(self, exclusions: Iterable[str] | str) -> list[str]:
         # if multiple items to exclude, combine with with item1|item2|...
+        hit = None
         exclude_string = "|".join(
-            re.escape(label) for label in itx.always_iterable(exclusions)
+            (hit := re.escape(label)) for label in itx.always_iterable(exclusions)
         )
+        if hit is None:
+            return []
         # regex to exclude subjects
-        return [f"^((?!({exclude_string})$).*)$"]
+        return [f"(?!({exclude_string})$)"]
 
 
 @attrs.define(slots=False)
