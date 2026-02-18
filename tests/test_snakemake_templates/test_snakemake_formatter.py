22cc6d36be0ac564ef07edd8c3d9dc5a1d2a450a
Peter Van Dyken <pvandyk2@uwo.ca>
2026-02-18 10:48:10 -0500

    Add tests for wildcard label correctness

--- tests/test_snakemake_templates/test_snakemake_formatter.py
+++ tests/test_snakemake_templates/test_snakemake_formatter.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import string
-from collections.abc import Collection
 
 import hypothesis.strategies as st
 import more_itertools as itx
@@ -12,46 +11,12 @@ from hypothesis import assume, example, given
 
 from snakebids.utils.snakemake_templates import SnakemakeFormatter
 from tests.helpers import Benchmark
-
-
-def _format_strings(
-    *,
-    base_exclude: set[str],
-    extra_exclude: Collection[str] | None = None,
-    min_size: int = 0,
-):
-    if extra_exclude is not None:
-        exclude = base_exclude | set(extra_exclude)
-    else:
-        exclude = base_exclude
-
-    return st.text(st.characters(exclude_characters=exclude), min_size=min_size)
-
-
-def field_names(
-    *, exclude_characters: Collection[str] | None = None, min_size: int = 0
-):
-    return _format_strings(
-        base_exclude=set("{},"), extra_exclude=exclude_characters, min_size=min_size
-    )
-
-
-def safe_field_names(
-    *, exclude_characters: Collection[str] | None = None, min_size: int = 0
-):
-    return _format_strings(
-        base_exclude=set("{},_[."), extra_exclude=exclude_characters, min_size=min_size
-    )
-
-
-def constraints(*, exclude_characters: Collection[str] | None = None):
-    return _format_strings(base_exclude=set("{}"), extra_exclude=exclude_characters)
-
-
-def literals(*, exclude_characters: Collection[str] | None = None):
-    return st.text(st.characters(exclude_characters=exclude_characters)).map(
-        lambda s: s.replace("{", "{{")
-    )
+from tests.test_snakemake_templates.strategies import (
+    constraints,
+    field_names,
+    literals,
+    safe_field_names,
+)
 
 
 class TestParserBenchmarks:
