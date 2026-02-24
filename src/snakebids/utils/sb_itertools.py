af681601f297338f124dc8eee2d45002a8b4c0c6
-g <pvandyk2@uwo.ca>
2026-02-24 09:47:50 -0500

    remove unused unpack function

--- src/snakebids/utils/sb_itertools.py
+++ src/snakebids/utils/sb_itertools.py
@@ -1,32 +1,8 @@
 import itertools as it
-from collections.abc import Iterable, Sequence
+from collections.abc import Iterable
 from typing import TypeVar
 
-import more_itertools as itx
-
 T = TypeVar("T")
-
-
-def unpack(iterable: Iterable[T], default: Sequence[T]) -> Iterable[T]:
-    """Return default if iterable has no elements.
-
-    Allows safe unpacking of possibly empty iterables
-
-    Parameters
-    ----------
-    iterable : Iterable[T]
-        Iterable to unpack
-    default : Sequence[T]
-        Values to return if then length of iterable is 0
-
-    Returns
-    -------
-    Iterable[T]
-    """
-    first, second = it.tee(iterable, 2)
-    if itx.ilen(first):
-        return second
-    return default
 
 
 def drop(n: int, iterable: Iterable[T]) -> Iterable[T]:
